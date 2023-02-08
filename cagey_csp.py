a352ead6b2240fe36ddedbdbd1c68c9efa0ea5bf
Aarushi Mathur <47922398+aa4mat@users.noreply.github.com>
2023-02-08 00:50:44 -0500

    nary_ad_grid added

--- cagey_csp.py
+++ cagey_csp.py
@@ -117,10 +117,10 @@ def binary_ne_grid(cagey_grid):
     domain = [v for v in range(1, grid_size+1)]
 
     # initialise Variables, add to CSP
-    vars = []
+    variables = []
     for i in product(range(1, grid_size+1), repeat=2):
         new_var = Variable(("Cell" + str(i)), domain)
-        vars.append(new_var)
+        variables.append(new_var)
         csp.add_var(new_var)
 
     # create scopes, create row constraint, add to CSP
@@ -130,7 +130,7 @@ def binary_ne_grid(cagey_grid):
             names = ["Cell(" + str(row) + ", " + str(i[0]) + ")",
                      "Cell(" + str(row) + ", " + str(i[1]) + ")"]
             scope_temp = []
-            for var in vars:   # get Variable that matches name, to add to scope
+            for var in variables:  # get Variable that matches name, to add to scope
                 # NOTE - creating a new variable won't work because matching
                 # done by object, not by Variable name
                 if var.name in names:
@@ -151,7 +151,7 @@ def binary_ne_grid(cagey_grid):
             names = ["Cell(" + str(i[0]) + ", " + str(col) + ")",
                      "Cell(" + str(i[1]) + ", " + str(col) + ")"]
             scope_temp = []
-            for var in vars:  # get Variable that matches name, to add to scope
+            for var in variables:  # get Variable that matches name, to add to scope
                 # NOTE - creating a new variable won't work because matching
                 # done by object, not by Variable name
                 if var.name in names:
@@ -168,7 +168,6 @@ def binary_ne_grid(cagey_grid):
                          permutations(range(1, grid_size + 1), 2)]
             cons.add_satisfying_tuples(sat_tuple)
             csp.add_constraint(cons)
-
     return csp, []  # right now we don't care about the grid
 
 
@@ -185,7 +184,70 @@ def nary_ad_grid(cagey_grid):
     # iterate - add sat_tuples([(1,2) ...
     # put it all into a CSP
 
-    pass
+    grid_size = cagey_grid[0]
+    csp = CSP("nary-allDiff-Cagey", [])  # initialise
+    domain = [v for v in range(1, grid_size + 1)]
+
+    # initialise Variables, add to CSP
+    variables = []
+    for i in product(range(1, grid_size + 1), repeat=2):
+        new_var = Variable(("Cell" + str(i)), domain)
+        variables.append(new_var)
+        csp.add_var(new_var)
+
+    # create scopes, create row n-ary constraint, add to CSP
+    for row in range(1, grid_size + 1):
+        row_scope = list()
+        for i in range(1, grid_size + 1):
+            names = list()
+            names.append("Cell({}, {})".format(row, i))
+            # names = ["Cell(" + str(row) + ", " + str(i[0]) + ")",
+            #          "Cell(" + str(row) + ", " + str(i[1]) + ")"]
+            scope_temp = []
+            for var in variables:
+                # get Variable that matches name, to add to scope
+                # NOTE - creating a new variable won't work because matching
+                # done by object, not by Variable name
+                if var.name in names:
+                    if not var in scope_temp:
+                        scope_temp.append(var)  # n values
+
+            row_scope.extend(scope_temp)
+            # create constraint for each ROW
+
+        cons = Constraint("N-ary-allDiff-Row({})".format(row), row_scope)
+
+        sat_tuple = [tup for tup in permutations(range(1, grid_size + 1), grid_size)]
+        cons.add_satisfying_tuples(sat_tuple)
+        csp.add_constraint(cons)
+
+    # create scopes, create column n-ary constraint, add to CSP
+    for col in range(1, grid_size + 1):
+        col_scope = list()
+        for i in range(1, grid_size + 1):
+            names = list()
+            names.append("Cell({}, {})".format(i, col))
+            scope_temp = []
+            for var in variables:
+                # get Variable that matches name, to add to scope
+                # NOTE - creating a new variable won't work because matching
+                # done by object, not by Variable name
+                if var.name in names:
+                    if not var in scope_temp:
+                        scope_temp.append(var)  # n values
+
+            col_scope.extend(scope_temp)
+            # create constraint for each COLUMN
+
+        cons = Constraint("N-ary-allDiff-Column({})".format(col), col_scope)
+
+        sat_tuple = [tup for tup in
+                     permutations(range(1, grid_size + 1), grid_size)]
+        cons.add_satisfying_tuples(sat_tuple)
+        csp.add_constraint(cons)
+
+    return csp, []  # right now we don't care about the grid
+
 
 def cagey_csp_model(cagey_grid):
     # things to do: construct a binary constraint
@@ -204,4 +266,99 @@ def cagey_csp_model(cagey_grid):
     # -> make helper fn for this
     # put it all into a CSP
 
-    pass
+    variables = []
+    size = cagey_grid[0][0]
+    n = size
+    csp = CSP("Kenken")
+    for i in range(1, n + 1):
+        row = []
+        for j in range(1, n + 1):
+            row.append(
+                Variable("%d%d" % (i, j), domain=list(range(1, n + 1))))
+        variables.append(row)
+    cons = []
+    kenken_len = len(cagey_grid)
+    for cage in range(1, kenken_len):
+        if (len(cagey_grid[cage]) == 2):
+            row_index = int(str(cagey_grid[cage][0])[0]) - 1
+            col_index = int(str(cagey_grid[cage][0])[1]) - 1
+            target_num = cagey_grid[cage][1]
+            variables[i][j] = Variable("%d%d" % (i, j), [target_num])
+        else:
+            operation = cagey_grid[cage][-1]
+            target_num = cagey_grid[cage][-2]
+            cage_vars = []
+            domain = []
+            for cell in range(len(cagey_grid[cage]) - 2):
+                row = int(str(cagey_grid[cage][cell])[0]) - 1
+                col = int(str(cagey_grid[cage][cell])[1]) - 1
+                cage_vars.append(variables[row][col])
+                domain.append(variables[row][col].domain())
+            cage_tuple = []
+            con = Constraint("cage%d" % (cage), cage_vars)
+            prod_domain = product(*domain)
+            for dom in prod_domain:
+                if (operation == 0):
+                    sum = 0
+                    for num in dom:
+                        sum += num
+                    if (sum == target_num):
+                        cage_tuple.append(dom)
+                elif (operation == 1):
+                    for num in permutations(dom):
+                        sub = num[0]
+                        for n in range(1, len(num)):
+                            sub -= num[n]
+                        if (sub == target_num):
+                            cage_tuple.append(dom)
+                elif (operation == 2):
+                    for num in permutations(dom):
+                        quo = num[0]
+                        for n in range(1, len(num)):
+                            quo = quo / num[n]
+                        if (quo == target_num):
+                            cage_tuple.append(dom)
+                elif (operation == 3):
+                    prod = 1
+                    for num in dom:
+                        prod *= num
+                    if (prod == target_num):
+                        cage_tuple.append(dom)
+            con.add_satisfying_tuples(cage_tuple)
+            cons.append(con)
+
+    for i in range(size):
+        for j in range(size):
+            for k in range(len(variables[i])):
+                if (k > j):
+                    row_var1 = variables[i][j]
+                    row_var2 = variables[i][k]
+                    con = Constraint(
+                        "r%d%d%d%d)" % (i + 1, j + 1, i + 1, k + 1),
+                        [row_var1, row_var2])
+                    row_tuples = []
+                    for dom in product(row_var1.domain(),
+                                       row_var2.domain()):
+                        if dom[0] != dom[1]:
+                            row_tuples.append(dom)
+                    con.add_satisfying_tuples(row_tuples)
+                    cons.append(con)
+                if (k > i):
+                    col_var1 = variables[i][j]
+                    col_var2 = variables[k][j]
+                    con = Constraint(
+                        "c%d%d%d%d)" % (i + 1, j + 1, k + 1, j + 1),
+                        [col_var1, col_var2])
+                    col_tuples = []
+                    for dom in product(col_var1.domain(),
+                                       col_var2.domain()):
+                        if dom[0] != dom[1]:
+                            col_tuples.append(dom)
+                    con.add_satisfying_tuples(col_tuples)
+                    cons.append(con)
+    for row in variables:
+        for var in row:
+            csp.add_var(var)
+    for con in cons:
+        csp.add_constraint(con)
+    return csp, variables
